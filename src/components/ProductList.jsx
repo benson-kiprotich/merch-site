580e87114b4860f5991733f24998209e66331993
Ben-Limo <bensokiplimo@gmail.com>
2024-11-19 16:22:16 +0300

    map products in the product list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Product from './Product';
 
 function ProductList({ productList }) {
-  return <div>ProductList</div>;
+  return (
+    <>
+      {productList.map((product) => (
+        <Product
+          name={product.name}
+          quantity={product.quantity}
+          description={product.description}
+        />
+      ))}
+    </>
+  );
 }
 
 ProductList.prototype = {

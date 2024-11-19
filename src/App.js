50c4f72f3277b7c5ba88689e36b39f695f168ea1
Ben-Limo <bensokiplimo@gmail.com>
2024-11-19 15:55:00 +0300

    add product controll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import ProductControl from './components/ProductControl';
 
 function App() {
   return (
     <>
       <Header />
+      <ProductControl />
     </>
   );
 }
---
+++ src/components/ProductControl.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ProductControl extends React.Component {
+  render() {
+    return <div>Product Control</div>;
+  }
+}
+
+export default ProductControl;

5d1c82ddb1899a9c5c4703467769c213707971d1
Ben-Limo <bensokiplimo@gmail.com>
2024-11-19 16:50:37 +0300

    add constructor to product control

--- src/components/ProductControl.jsx
+++ src/components/ProductControl.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class ProductControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      mainProductList: [],
+    };
+  }
+
   render() {
     return <div>Product Control</div>;
   }

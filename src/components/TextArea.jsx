da9aa6598fb4b597413a7699a0d99238cf9e3e83
Ben-Limo <bensokiplimo@gmail.com>
2024-11-22 09:47:46 +0300

    add text area component

---
+++ src/components/TextArea.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const TextArea = styled.textarea`
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+  height: 8rem;
+  box-sizing: border-box;
+`;
+
+export default TextArea;

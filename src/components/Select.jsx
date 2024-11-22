9a1681fabeb7540b9a9ac2f35e982712725a7e87
Ben-Limo <bensokiplimo@gmail.com>
2024-11-22 09:23:24 +0300

    add select component

---
+++ src/components/Select.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Select = styled.select`
+  width: 100%;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  font-size: 1rem;
+  box-sizing: border-box;
+`;
+
+export default Select;

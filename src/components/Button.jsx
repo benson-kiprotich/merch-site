e7667c16279f725edefd743fe694c590df39851e
Ben-Limo <bensokiplimo@gmail.com>
2024-11-22 09:24:31 +0300

    add button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  font-size: 1rem;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+export default Button;

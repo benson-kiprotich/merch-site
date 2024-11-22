d85b349238517fd96f3e29363b8a4f4813042cf9
Ben-Limo <bensokiplimo@gmail.com>
2024-11-22 09:22:24 +0300

    add input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  font-size: 1rem;
+  box-sizing: border-box;
+`;
+
+export default Input;

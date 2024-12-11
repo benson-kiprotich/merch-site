c48e02887164f5c3f61fb9676762c8bce092f7bd
Ben-Limo <bensokiplimo@gmail.com>
2024-12-11 16:43:11 +0300

    add func and test for adding new product to the main list

--- src/__tests__/reducers/product-list-reducer.test.js
+++ src/__tests__/reducers/product-list-reducer.test.js
@@ -1,7 +1,35 @@
 import productListReducer from '../../reducers/product-list-reducer';
 
 describe('productListReducer', () => {
+  let action;
+  const productData = {
+    name: 'test product',
+    quantity: 3,
+    description: 'test description',
+    id: 1,
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(productListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new product data to mainProductList', () => {
+    const { name, quantity, description, id } = productData;
+    action = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      quantity: quantity,
+      description: description,
+      id: id,
+    };
+
+    expect(productListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        quantity: quantity,
+        description: description,
+        id: id,
+      },
+    });
   });
 });
--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,5 +1,19 @@
 const reducer = (state = {}, action) => {
-  return state;
+  const { name, quantity, description, id } = action;
+  switch (action.type) {
+    case 'ADD_PRODUCT':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          quantity: quantity,
+          description: description,
+          id: id,
+        },
+      });
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
